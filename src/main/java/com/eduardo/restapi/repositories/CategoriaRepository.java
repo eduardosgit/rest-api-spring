3be73e5b601f36d550bbe81561f6a0f10a195902
Eduardo Rodrigues <eduardosrodrigues@outlook.com>
2020-02-25 15:34:16 -0300

    Criado Repository e Service para Categoria

---
+++ src/main/java/com/eduardo/restapi/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.eduardo.restapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduardo.restapi.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+	
+
+}
---
+++ src/main/java/com/eduardo/restapi/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.eduardo.restapi.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardo.restapi.domain.Categoria;
+import com.eduardo.restapi.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+}
